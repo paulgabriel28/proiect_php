eb61a47da87b3e793baf66be07a387d1691d4f22
paulgabriel28 <128238707+paulgabriel28@users.noreply.github.com>
2023-08-28 11:20:15 +0300

    Update database.php
    update info

--- config/database.php
+++ config/database.php
@@ -1,6 +1,12 @@
 <?php
 
-const DB_HOST = 'localhost';
-const DB_NAME = 'paulgabriel.ro';
-const DB_USER = 'root';
-const DB_PASSWORD = '';
+const DB_HOST = 'DB_HOST';
+const DB_NAME = 'DB_NAME';
+const DB_USER = 'DB_USERNAME';
+const DB_PASSWORD = 'DB_PASSWORD';
+
+// // for localhost:
+// const DB_HOST = 'localhost';
+// const DB_NAME = 'DB_NAME';
+// const DB_USER = 'root';
+// const DB_PASSWORD = '';
